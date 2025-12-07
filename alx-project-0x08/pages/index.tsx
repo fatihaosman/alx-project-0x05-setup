afea24508ad02b5886c6f8184a17d5cb7191396d
fatihaosman <fatihaosman17@gmail.com>
2025-12-07 20:44:36 +0300

    added folder 8 and 9

--- alx-project-0x08/pages/index.tsx
+++ alx-project-0x08/pages/index.tsx
@@ -37,7 +37,7 @@
 
 
 
-import ImageCard from "@/components/common/imageCard";
+import ImageCard from "@/components/common/ImageCard.tsx";
 import { ImageProps } from "@/interfaces";
 import { useState } from "react";
 
